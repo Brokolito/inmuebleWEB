5508a8bd47aacbfd8ceac7671b2ec33532b3b3fe
Brokolito <m.castro14@ufromail.cl>
2023-06-22 09:46:31 -0400

    Se modifcan cositas

--- src/main/java/com/inmueble/inmueblesweb/model/data/dao/vendedorDAO.java
+++ src/main/java/com/inmueble/inmueblesweb/model/data/dao/vendedorDAO.java
@@ -2,10 +2,24 @@ package com.inmueble.inmueblesweb.model.data.dao;
 
 import com.inmueble.inmueblesweb.model.Vendedor;
 import org.jooq.DSLContext;
+import org.jooq.impl.DSL;
 
 public class vendedorDAO {
 
 	public boolean agregarVendedor(Vendedor vendedor, DSLContext query) {
-		throw new UnsupportedOperationException();
+		String rut=vendedor.getRut();
+		String nombre=vendedor.getNombre();
+		String estadoCivil=vendedor.getEstadoCivil();
+		String direccion=vendedor.getDireccion();
+		String tituloProfesional=vendedor.getTituloProfesional();
+		int result=0;
+		try {
+			result=query.insertInto(DSL.table("vendedor"),
+					DSL.field("rut"),DSL.field("nombre"),DSL.field("estado_civil"),DSL.field("direccion"),DSL.field("titulo_profesional"))
+					.values(rut,nombre,estadoCivil,direccion,tituloProfesional).execute();
+		}catch (Exception e){
+			e.printStackTrace();
+		}
+		return result==1;
 	}
 }
